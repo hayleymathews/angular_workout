5d20057f188d23883555d107245422caeaae3392
yy-math <yy.mathews@gmail.com>
2016-08-01 18:31:01 -0500

    new repo for this

---
+++ js/app.js
@@ -0,0 +1,15 @@
+angular.module('app', ['ngRoute', '7minWorkout'])
+    .config(function ($routeProvider, $sceDelegateProvider) {
+        $routeProvider.when('/start', 
+            {templateUrl: 'partials/start.html'});
+        $routeProvider.when('/workout', 
+            {templateUrl: 'partials/workout.html', controller: 'WorkoutController'});
+        $routeProvider.when('/finish', 
+            {templateUrl: 'partials/finish.html'});
+        $routeProvider.otherwise(
+            {redirectTo: '/start'});
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'http://*.youtube.com/**']);
+    });
+angular.module('7minWorkout', []);
